9d9d28230b13c797196839af7e7001e41da9861e
DImitar Dimitrov <32570431+Dimodim@users.noreply.github.com>
2020-01-09 23:28:19 +0200

    Update Function and statements.js

--- Syntax, Functions and Statements/Function and statements.js
+++ Syntax, Functions and Statements/Function and statements.js
@@ -19,13 +19,13 @@ function gcd_two_numbers(x, y) {
 
   function  sameNumbersInNumbeer(num){
     
-    numAsString = String(num).split("");
+     numAsString = String(num).split("");
 
-    let digit = numAsString[0];
-    let sameNumbers = true;
+     let digit = numAsString[0];
+     let sameNumbers = true;
 
-    for(let i = 0 ; i < num.length ;i++ ){
-      sumNums+=sum[i];
+     for(let i = 0 ; i < num.length ;i++ ){
+        sumNums+=sum[i];
 
         if ( num[i] != digit ){
             sameNumbers = false;
@@ -37,9 +37,52 @@ function gcd_two_numbers(x, y) {
         console.log(sameNumbers);
         return console.log(sum); 
 
-  } 
+    } 
     
 
-    sameNumbersInNumbeer(22222222222);
+ function  sameNumbersInNumbeer(num){
+    
+  numAsString = String(num).split("");
+
+  let digit = numAsString[0];
+  let sameNumbers = true;
+
+  for(let i = 0 ; i < num.length ;i++ ){
+    sumNums+=sum[i];
+
+    if ( num[i] != digit ){
+        sameNumbers = false;
+        break;
+    }
+  }
+  
+  let sum = numAsString.map(Number).reduce((accumulator, currentValue) => accumulator + currentValue);
+  console.log(sameNumbers);
+  return console.log(sum); 
+
+} 
+
+function TimeToWalk(arg1, arg2, arg3) {
+  let stepsNumber = Number(arg1);
+  let stepsMetersHr = Number(arg2);
+  let studentSpeed = Number(arg3);
+
+  let distanceMeters = stepsNumber * stepsMetersHr;
+  let speedMetersSec = studentSpeed / 3.6;
+  let time = distanceMeters / speedMetersSec;
+  let rest = Math.floor(distanceMeters / 500);
+
+  let timeMin = Math.floor(time / 60);
+  let timeSec = Math.round(time - (timeMin * 60));
+  let timeHr = Math.floor(time / 3600);
+
+
+  console.log((timeHr < 10 ? "0" : "") + timeHr + ":" 
+  + (timeMin + rest < 10 ? "0" : "") 
+  + (timeMin + rest) + ":" 
+  + (timeSec < 10 ? "0" : "") 
+  + timeSec);
+
+}
 
 
